0257f157c91d7443cafd9c67f9f551c2b44ca8fb
xieyadong <jxieyadong@hc360.com>
2021-08-02 14:28:44 +0800

    update metadata for hex publish

--- mix.exs
+++ mix.exs
@@ -5,9 +5,14 @@ defmodule SimpleHttpc.MixProject do
     [
       app: :simple_httpc,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.7",
+      description: "a simple http client",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: [
+        licenses: ["Public Domain"],
+        links: %{"GitHub" => "https://github.com/hsieh/simple_httpc"}
+      ]
     ]
   end
 
@@ -21,8 +26,7 @@ defmodule SimpleHttpc.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
